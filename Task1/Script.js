fc4b0e74d83ac59b4314f45e78e15b460e82c6bb
mohammed-n-jumaa <mohammed.n.jumaa@gmail.com>
2024-09-25 00:17:38 +0300

    pushing js Task

---
+++ Task1/Script.js
@@ -0,0 +1,137 @@
+//Data Types Task
+//.........1..........
+let cash = 1000;
+let Liabilities = 500;
+let cashFlowRatio = cash / Liabilities;
+console.log("Cash flow ratio: ", cashFlowRatio); 
+
+let revenues = 1000;
+let expenses = 500;
+let netIncome = revenues - expenses;
+console.log("Net income: ", netIncome); 
+
+let liabilities = 1000;
+let equity = 500;
+let totalAssets = liabilities + equity;
+console.log("Total assets: ", totalAssets); 
+
+let profit = 1000;
+let sales = 500;
+let netIncomeFromMargin = profit * sales;
+console.log("Net income (from margin and sales): ", netIncomeFromMargin); 
+
+let numbers = [7, 9, 2];
+let average = numbers.reduce((a, b) => a + b, 0) / numbers.length;
+console.log("Average: ", average); // 
+
+let price = 150;
+let discount = 0.30; 
+let discountedPrice = price - (price * discount);
+console.log("Discounted price: ", discountedPrice); 
+
+let age = 20;
+let isWithinAgeLimit = age > 18 && age < 30;
+console.log("Is age within the limit (older than 18 and less than 30): ", isWithinAgeLimit); 
+
+let base = 2;
+let exponent = 3;
+let exponentialResult = Math.pow(base, exponent);
+console.log("Exponential result (2^3): ", exponentialResult); 
+
+let number1 = 10;
+let number2 = 3;
+let remainder = number1 % number2;
+console.log("Remainder of 10 divided by 3: ", remainder); 
+
+//..................................................................................
+//.................2.....................
+let x = typeof (100);
+console.log(x, "Type of 100");
+
+let y = typeof (73.9);
+console.log (y, "Type of 73.9");
+
+let m =typeof ("NaN");
+console.log (m, "Type of NaN");
+
+let z =typeof ("Water");
+console.log (z, 'Type of "Water"');
+
+let q =typeof (10 > 9);
+console.log (q, "Type of false");
+
+let w =typeof (9 != 11);
+console.log (w, "Type of 9 != 11");
+
+let  i=typeof ("Orang" + "e");
+console.log (i, "Type of Orang  +  e");
+
+let  u=typeof (" Orange - s ");
+console.log (u, "Type of  Orange - s ");
+
+let result1 = typeof ("4" + "8");
+console.log(result1, 'Type of "4" + "8"');
+
+let result2 = typeof ("4" - "8");
+console.log(result2, 'Type of "4" - "8"');
+
+let result3 = typeof ("name" + 3);
+console.log(result3, 'Type of "name" + 3');
+
+let result4 = typeof ("name" - 3);
+console.log(result4, 'Type of "name" - 3');
+
+let result5 = typeof (82 * "word");
+console.log(result5, 'Type of 82 * "word"');
+
+let result6 = typeof (1 + "hello");
+console.log(result6, 'Type of 1 + "hello"');
+
+let result7 = typeof ("hello" + 1);
+console.log(result7, 'Type of "hello" + 1');
+
+let result8 = typeof (1 + true);
+console.log(result8, 'Type of 1 + true');
+
+let result9 = typeof ("hello" + true);
+console.log(result9, 'Type of "hello" + true');
+
+let result10 = typeof Infinity;
+console.log(result10, 'Type of Infinity');
+
+let result11 = typeof (1 == '1');
+console.log(result11, 'Type of 1 == "1"');
+
+let result12 = typeof (1 === '1');
+console.log(result12, 'Type of 1 === "1"');
+//............................................
+//String
+//.................1................
+let str = "Welcome to Orange";
+
+console.log(str.toUpperCase());
+
+console.log(str.substring(8, 10));
+
+console.log(str.replace("Welcome to", "Hello from"));
+
+console.log(str.toLowerCase());
+
+console.log(str.length);
+
+console.log(str.replace("Orange", `"Orange"`));
+
+console.log(str + " Jordan");
+//................2.......................
+function replaceFirstLetterOccurrences(str) {
+    if (str.length === 0) return str; 
+    let firstLetter = str[0]; 
+    let result = firstLetter + str.slice(1).replaceAll(firstLetter, '*');
+    return result;
+}
+
+let input = "cactus";
+let output = replaceFirstLetterOccurrences(input);
+console.log(output);
+
+
---
+++ Task1/main.js
@@ -0,0 +1,143 @@
+//.......................Arrays...................
+//..........................1.....................
+let array = ["Coding", "Academy", "By", "Orange"];
+
+let arrayWithJordan = [...array, "Jordan"];
+console.log(arrayWithJordan);
+
+let firstTwoElements = array.slice(0, 2);
+console.log(firstTwoElements);
+
+let arrayWithWelcome = ["Welcome", "To", ...array];
+console.log(arrayWithWelcome);
+
+let excludeFirstElement = array.slice(1);
+console.log(excludeFirstElement);
+
+let arrayAsString = array.join(" ");
+console.log(arrayAsString);
+
+console.log(array);
+
+let codingAndOrange = [array[0], array[array.length - 1]];
+console.log(codingAndOrange);
+//..........................2.....................
+
+var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
+vegetables.pop(); 
+console.log(vegetables); 
+//...............
+var fruit = ["banana", "apple", "orange", "watermelon"];
+fruit.shift(); 
+console.log(fruit); 
+//.................
+var fruit = ["apple", "orange", "watermelon"];
+var orangeIndex = fruit.indexOf("orange");
+console.log(orangeIndex,"is index of orange"); 
+//.................
+var fruit = ["apple", "orange", "watermelon"];
+var orangeIndex = fruit.indexOf("orange");
+fruit.push(orangeIndex); 
+console.log(fruit); 
+//.................
+var vegetables = ["carrot", "tomato", "pepper"];
+var vegetableLength = vegetables.length;
+console.log(vegetableLength,"is length of the vegetable array"); 
+//.................
+var vegetables = ["carrot", "tomato", "pepper"];
+var vegetableLength = vegetables.length;
+vegetables.push(vegetableLength); 
+console.log(vegetables); 
+//.................
+var fruit = ["apple", "orange", "watermelon", 1];
+var vegetables = ["carrot", "tomato", "pepper", 3];
+var food = fruit.concat(vegetables);
+console.log(food); 
+//.................
+var food = ["apple", "orange", "watermelon", 1, "carrot", "tomato", "pepper", 3];
+food.splice(4, 2); 
+console.log(food); 
+//.................
+var food = ["apple", "orange", "watermelon", 1, "pepper", 3];
+food.reverse();
+console.log(food); 
+//.................
+var food = [3, "pepper", 1, "watermelon", "orange", "apple"];
+var foodString = food.join(", ");
+console.log(foodString); // "3, pepper, 1, watermelon, orange, apple"
+//...........................................................................
+//.......................Function............................................
+//...........................1...............................................
+function splitStringIntoArray(str) {
+    return str.split(" ");
+}
+console.log(splitStringIntoArray("Orange Jordan")); 
+//...........................2...............................................
+function hidePhoneNumber(phoneNumber) {
+    return phoneNumber.slice(0, 7).replace(/\d/g, '*') + phoneNumber.slice(7);
+}
+console.log(hidePhoneNumber("0776807777")); 
+//...........................3...............................................
+function hideEmail(email) {
+    let [local, domain] = email.split("@");
+    return local.slice(0, 6) + '…@' + domain;
+}
+console.log(hideEmail("orange_academy@orange.jo")); 
+//............................4..............................................
+function capitalizeFirstWord(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capitalizeFirstWord("coding academy by orange")); 
+//............................5..............................................
+function capitalizeEachWord(str) {
+    return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+}
+console.log(capitalizeEachWord("coding academy by orange")); 
+//............................6..............................................
+function flipNumber(num) {
+    return parseInt(num.toString().split('').reverse().join(''));
+}
+console.log(flipNumber(92485)); 
+//............................7..............................................
+function swapWithDestructuring(a, b) {
+    [a, b] = [b, a];
+    return [a, b];
+}
+
+let a = 3;
+let b = 4;
+[a, b] = swapWithDestructuring(a, b);
+console.log("After swap: a =", a, ", b =", b);
+
+//.............................8.............................................
+function removeCharacterAt(str, index) {
+    return str.slice(0, index) + str.slice(index + 1);
+}
+console.log(removeCharacterAt("Orange", 3)); 
+//.............................9..............................................
+function mergeWithoutFirstCharacter(str1, str2) {
+    return str1.slice(1) + str2.slice(1);
+}
+console.log(mergeWithoutFirstCharacter("lora", "inge")); 
+//.............................10..............................................
+function isCharInFirstOrLastPosition(char, str) {
+    return str.startsWith(char) || str.endsWith(char);
+}
+console.log(isCharInFirstOrLastPosition("o", "orange")); 
+console.log(isCharInFirstOrLastPosition("z", "orange")); 
+//..............................11.............................................
+function wordsToArray(str) {
+    return str.split(" ");
+}
+console.log(wordsToArray("Coding Academy by Orange")); 
+//...............................12.............................................
+function sortCharacters(str) {
+    return str.split("").sort().join("");
+}
+console.log(sortCharacters("Orange")); 
+
+
+
+
+
+
